4443e3cc48573618a544f69aea1e066bff58059b
hay-0le <haley.olsen1@gmail.com>
2020-01-26 22:34:14 -0800

    Added ImageLoader component to display default loader image while movie image loads

--- client/src/components/Movies.jsx
+++ client/src/components/Movies.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MoviesDiv, Movie} from "./componentStyling/MovieStyling.js";
+import ImageLoader from 'react-load-image';
 
 function Movies (props) {
   var movies = props.movies;
@@ -10,7 +11,11 @@ function Movies (props) {
       {[...movies].map((movie, i) =>
 
         <Movie key={i} onClick={click}>
-          <img id={movie._id} src={(movie.pictureURL)} height="113" width="76" />
+          <ImageLoader src={(movie.pictureURL)}>
+            <img id={movie._id}  height="113" width="76" />
+            <div>Error Loading Image</div>
+            <img src="../imageLoading.png" height="113" width="76"/>
+          </ImageLoader>
         </Movie>
       )}
     </MoviesDiv>
